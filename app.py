96137765f1dfd36516e87477026564bffe6bf662
Vevade Deploy <deploy@vevade.com>
2026-03-15 00:54:16 -0400

    Remove cta_text from promo banner

--- app.py
+++ app.py
@@ -85,7 +85,6 @@ def get_promo():
         "title": "Spring Cleanup Special - 15% OFF!",
         "subtitle": "Book by March 1st to save",
         "discount_text": "15% OFF",
-        "cta_text": "Claim Offer",
         "deadline_date": "2026-03-01"
     })
 
